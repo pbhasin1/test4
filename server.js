6d80d901fcbc5c1f2b7170c6f5b8bfd65551eb12
pbhasin1 <pbhasin@sapient.com>
2017-03-27 16:04:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 var pg = require('pg');
 
-var conString="postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6b7de7f2584d7c5994b67c39b7db665db@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d4u4oil2mtd4f6";
+var connectionString="postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6b7de7f2584d7c5994b67c39b7db665db@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d4u4oil2mtd4f6";
 
 
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    console.log('Error connecting to Postgres');
-    console.log('err', err);
-    return 1;
-  } else {
-    console.log("Successfully connected");
-    client.query("select id,firstname,lastname from salesforce2.contact", function(err, result){
-      done();
-      if(err) {
-        console.log('Error reading from Postgres');
-        return 1;
-      }
-      else{
-        console.log('Results: ', result);
-      }
-    });
+
+//We access a PostgreSQL client
+
+//We use the 'pg' module's recommended client pooling API
+//We pass the connect function the database connection string, and a callback function
+//'onConnect'. We define that function.
+pg.connect(connectionString, onConnect);
+
+function onConnect(err, client, done) {
+  //Err - This means something went wrong connecting to the database.
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-});
+
+  //For now let's end client
+  client.end();
+}
