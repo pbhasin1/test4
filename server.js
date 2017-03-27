9a3c1374a30b0aff25a5bf7e02542df8e7a57a3b
pbhasin1 <pbhasin@sapient.com>
2017-03-27 16:13:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var pg = require('pg');
 
 var connectionString="postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6b7de7f2584d7c5994b67c39b7db665db@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d4u4oil2mtd4f6";
 
-
+var express = require("express");
+var app = express();
+app.use(express.logger());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 //We access a PostgreSQL client
 
@@ -11,13 +20,11 @@ var connectionString="postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6
 //'onConnect'. We define that function.
 pg.connect(connectionString, onConnect);
 
-function onConnect(err, client, done) {
-  //Err - This means something went wrong connecting to the database.
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT id FROM salesforce2.contact', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
 
-  //For now let's end client
-  client.end();
-}
