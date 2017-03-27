8b782463f144c4e8d8cde112b80fe4d662c9600e
pbhasin1 <pbhasin@sapient.com>
2017-03-27 15:29:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const pg = require('pg');
-const path = require('path');
-const connectionString = 'postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6b7de7f2584d7c5994b67c39b7db665db@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d4u4oil2mtd4f6';
-router.get('/api/v1/todos', (req, res, next) => {
-  const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      alert('hi');
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('select id,firstname,lastname from salesforce2.contact;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+var connectionString = 'postgres://zchnevkljossds:3d31d8e87ff40a8f1ae4ef76d24893a6b7de7f2584d7c5994b67c39b7db665db@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d4u4oil2mtd4f6';
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/update', function(req, res) {
+    pg.connect(connectionString, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query('select id,firstname,lastname from salesforce2.contact',
+            [req.body.firstname.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                 function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+           
+        );
     });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
