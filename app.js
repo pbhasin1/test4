9cdb0d214d9c5b9078e3eba4008b7c1776b6662b
pbhasin1 <pbhasin@sapient.com>
2017-04-24 15:02:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var pg = require('pg');
 var app = express();
 var server;
+var crypto = require('crypto'); 
+
 var connectionString = 'postgres://tijurpxfuncvvn:4d844b13e61da666b5055e284a696e75d6c8e41033dc2098cdb352f8708b4a64@ec2-54-243-185-123.compute-1.amazonaws.com:5432/d5q988qc4a2f8a';
 
 var start = exports.start = function start(port, callback) {
@@ -63,7 +65,124 @@ app.get('/addRecord', function sendResponse(req,res) {
             res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS");
             res.header("Access-Control-Allow-Origin","*");
             res.status(200).send('contact added');
-    });    
+    });
+var request = require('request');
+var OAuth   = require('oauth-1.0a');
+var crypto  = require('crypto');
+
+var oauth = OAuth({
+    consumer: {
+        key: '5m5nwqmjremo0kqe4p1ztu3xme5tgrkrqxaomayq',
+        secret: 'uzvua0aafhpv0crr01cevmeeddngyuptwlppmaji'
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: function(base_string, key) {
+        return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+    }
+});
+app.get('/oAuthMethod', function sendResponse(req,res) {
+var request_data = {
+    url: 'https://apisandbox.openbankproject.com/oauth/initiate',
+    method: 'POST',
+    data: {
+        
+    }
+};
+request({
+    url: 'https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=',
+    method: request_data.method,
+    form: request_data.data,
+    headers:    oauth.toHeader(oauth.authorize(request))
+}, function(error, response, body) {
+    alert('got the response');
+});
+});
+/*var http = require('http');
+var OAuth = require('../oauth.js').OAuth;
+var nodeUrl = require('url');
+var clientID = '';
+var clientSecret = '';
+var callbackURL = '';
+
+oa = new OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    clientID,
+    clientSecret,
+    '1.0',
+    callbackURL,
+    'HMAC-SHA1'
+);
+
+http.createServer(function (request, response) {
+    oa.getOAuthRequestToken(function (error, oAuthToken, oAuthTokenSecret, results) {
+        var urlObj = nodeUrl.parse(request.url, true);
+        var authURL = 'https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=' + oAuthToken;
+        var handlers = {
+            '/': function (request, response) {
+                //
+                 // Creating an anchor with authURL as href and sending as response
+                 
+                var body = '<a href="' + authURL + '"> Get Code </a>';
+                response.writeHead(200, {
+                    'Content-Length': body.length,
+                    'Content-Type': 'text/html' });
+                response.end(body);
+            },
+            '/callback': function (request, response) {
+                // Obtaining access_token 
+                var getOAuthRequestTokenCallback = function (error, oAuthAccessToken,
+                                                             oAuthAccessTokenSecret, results) {
+                    if (error) {
+                        console.log(error);
+                        response.end(JSON.stringify({
+                            message: 'Error occured while getting access token',
+                            error: error
+                        }));
+                        return;
+                    }
+
+                    oa.get('https://apisandbox.openbankproject.com/oauth/initiate',
+                           oAuthAccessToken,
+                           oAuthAccessTokenSecret,
+                           function (error, twitterResponseData, result) {
+                               if (error) {
+                                   console.log(error)
+                                   res.end(JSON.stringify(error));
+                                   return;
+                               }
+                               try {
+                                   console.log(JSON.parse(twitterResponseData));
+                               } catch (parseError) {
+                                   console.log(parseError);
+                               }
+                               console.log(twitterResponseData);
+                               response.end(twitterResponseData);
+                           });
+                };
+
+                oa.getOAuthAccessToken(urlObj.query.oauth_token, oAuthTokenSecret,
+                                       urlObj.query.oauth_verifier,
+                                       getOAuthRequestTokenCallback);
+
+            }
+        };
+        handlers[urlObj.pathname](request, response);
+    })
+
+}).listen(3000);*/
+/*app.get('/oauthMethod', function sendResponse(req,res) {
+var oauth = OAuth({
+    consumer: {
+        key: '5m5nwqmjremo0kqe4p1ztu3xme5tgrkrqxaomayq',
+        secret: 'uzvua0aafhpv0crr01cevmeeddngyuptwlppmaji'
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: function(base_string, key) {
+        return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+    }
+});
+});*/
 //app.get('/db', function sendResponse(req,res) {
     //res.status(200).send("Database Data Placeholder");
 //});
