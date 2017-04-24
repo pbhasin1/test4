341a37e1c5f2be0b3e4caa4dc59220bb6e5c0914
pbhasin1 <pbhasin@sapient.com>
2017-04-24 15:14:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -66,38 +66,56 @@ app.get('/addRecord', function sendResponse(req,res) {
             res.header("Access-Control-Allow-Origin","*");
             res.status(200).send('contact added');
     });
-var request = require('request');
-var OAuth   = require('oauth-1.0a');
-var crypto  = require('crypto');
+var expect  = require('chai').expect;
+var Request = require('request');
+var OAuth   = require('../../oauth-1.0a');
+var crypto = require('crypto');
 
-var oauth = OAuth({
-    consumer: {
-        key: '5m5nwqmjremo0kqe4p1ztu3xme5tgrkrqxaomayq',
-        secret: 'uzvua0aafhpv0crr01cevmeeddngyuptwlppmaji'
-    },
-    signature_method: 'HMAC-SHA1',
-    hash_function: function(base_string, key) {
-        return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-    }
-});
-var request_data = {
-    url: 'https://apisandbox.openbankproject.com/oauth/initiate',
-    method: 'POST',
-    data: {
-        
-    }
-};
-app.get('/oAuthMethod', function sendResponse(req,res) {
+describe.skip("Openbank Personal Consumer", function() {
+    this.timeout(10000);
 
-request({
-    url: 'https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=',
-    method: request_data.method,
-    form: request_data.data,
-    headers:    oauth.toHeader(oauth.authorize(request))
-}, function(error, response, body) {
-    alert('got the response');
+    var oauth = new OAuth({
+        consumer: {
+            key: process.env.OPENBANK_CONSUMER_PUBLIC,
+            secret: process.env.OPENBANK_CONSUMER_SECRET
+        },
+        signature_method: 'HMAC-SHA256',
+        hash_function: function(base_string, key) {
+            return crypto.createHmac('sha256', key).update(base_string).digest('base64');
+        }
+    });
+
+    //need to send as header
+    describe("#Request Token", function() {
+        var request = {
+            url:    'https://apisandbox.openbankproject.com/oauth/initiate',
+            method: 'POST',
+            data: {
+                oauth_callback: 'http://www.ddo.me'
+            }
+        };
+
+        it("should be a valid response", function(done) {
+            Request({
+                url:        request.url,
+                method:     request.method,
+                form:       request.data,
+                headers:    oauth.toHeader(oauth.authorize(request))
+            }, function(err, res, body) {
+                expect(body).to.be.a('string');
+
+                body = oauth.deParam(body);
+
+                expect(body).to.have.property('oauth_callback_confirmed', 'true');
+                expect(body).to.have.property('oauth_token');
+                expect(body).to.have.property('oauth_token_secret');
+
+                done();
+            });
+        });
+    });
 });
-});
+
 /*var http = require('http');
 var OAuth = require('../oauth.js').OAuth;
 var nodeUrl = require('url');
