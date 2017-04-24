8ece72cc3f146bbe4c7d155d61eb1dd1b244c17f
pbhasin1 <pbhasin@sapient.com>
2017-04-24 16:22:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,392 @@
-var app = require('./app');
-var http = require('http');
-var OAuth = require('./oauth-1.0a.js').OAuth;
-var nodeUrl = require('url');
-var clientID = '5m5nwqmjremo0kqe4p1ztu3xme5tgrkrqxaomayq';
-var clientSecret = 'uzvua0aafhpv0crr01cevmeeddngyuptwlppmaji';
-var callbackURL = 'www.google.com';
+var express = require('express', template = require('pug'));
+var session = require('express-session')
+var util = require('util');
+var oauth = require('oauth');
 
-oa = new OAuth(
-    'https://apisandbox.openbankproject.com/oauth/initiate',
-    'https://apisandbox.openbankproject.com/oauth/initiate',
-    clientID,
-    clientSecret,
-    '1.0',
-    callbackURL,
-    'HMAC-SHA1'
-);
+var app = express();
 
-http.createServer(function (request, response) {
-    oa.getOAuthRequestToken(function (error, oAuthToken, oAuthTokenSecret, results) {
-        var urlObj = nodeUrl.parse(request.url, true);
-        var authURL = 'https://apisandbox.openbankproject.com/oauth/authorize?oauth_token=' + oAuthToken;
-        var handlers = {
-            '/': function (request, response) {
-                //
-                 // Creating an anchor with authURL as href and sending as response
-                 
-                var body = '<a href="' + authURL + '"> Get Code </a>';
-                response.writeHead(200, {
-                    'Content-Length': body.length,
-                    'Content-Type': 'text/html' });
-                response.end(body);
-            },
-            '/callback': function (request, response) {
-                // Obtaining access_token 
-                var getOAuthRequestTokenCallback = function (error, oAuthAccessToken,
-                                                             oAuthAccessTokenSecret, results) {
-                    if (error) {
-                        console.log(error);
-                        response.end(JSON.stringify({
-                            message: 'Error occured while getting access token',
-                            error: error
-                        }));
-                        return;
+// To get the values for the following fields, please register your client here:
+// https://apisandbox.openbankproject.com/consumer-registration
+// Then create a file called config.json in this directory 
+// and paste your consumer key and secret like this:
+//config.json:
+//{ 
+//"consumerKey": "YOUR CONSUMER KEY GOES HERE",
+//"consumerSecret" : "YOUR CONSUMER SECRET GOES HERE"
+//}
+
+// Template engine (previously known as Jade)
+var pug = require('pug');
+
+// This loads your consumer key and secret from a file you create.
+//var config = require('./config.json');
+
+// Used to validate forms
+var bodyParser = require('body-parser')
+
+
+// create application/x-www-form-urlencoded parser 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+var _openbankConsumerKey = '5m5nwqmjremo0kqe4p1ztu3xme5tgrkrqxaomayq';
+var _openbankConsumerSecret = 'uzvua0aafhpv0crr01cevmeeddngyuptwlppmaji';
+
+
+// The location, on the interweb, of the OBP API server we want to use.
+var apiHost = 'https://apisandbox.openbankproject.com';
+
+console.log ("apiHost is: " + apiHost)
+
+
+var consumer = new oauth.OAuth(
+  apiHost + '/oauth/initiate',
+  apiHost + '/oauth/token',
+  _openbankConsumerKey,
+  _openbankConsumerSecret,
+  '1.0',                             //rfc oauth 1.0, includes 1.0a
+  'http://127.0.0.1:8085/callback',
+  'HMAC-SHA1');
+
+var cookieParser = require('cookie-parser');
+app.use(session({
+  secret: "very secret",
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+app.get('/connect', function(req, res){
+  consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.status(500).send("Error getting OAuth request token : " + util.inspect(error));
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect(apiHost + "/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
+    }
+  });
+});
+
+
+app.get('/callback', function(req, res){
+  consumer.getOAuthAccessToken(
+    req.session.oauthRequestToken,
+    req.session.oauthRequestTokenSecret,
+    req.query.oauth_verifier,
+    function(error, oauthAccessToken, oauthAccessTokenSecret, result) {
+      if (error) {
+        //oauthAccessToken, -Secret and result are now undefined
+        res.status(500).send("Error getting OAuth access token : " + util.inspect(error));
+      } else {
+        //error is now undefined
+        req.session.oauthAccessToken = oauthAccessToken;
+        req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+        res.redirect('/signed_in');
+      }
+    }
+  );
+});
+
+
+app.get('/signed_in', function(req, res){
+
+  var template = "./template/signedIn.pug"
+  var options = {}
+  var html = pug.renderFile(template, options)
+  res.status(200).send(html)
+
+   // 'Signing in by OAuth worked. Now you can do API calls on private data like this: <br><a href="/getMyAccounts">Get My Accounts</a> <br><a href="/getCurrentUser">Get Current User</a> <br><a href="/createTransactionRequest">Create Transaction Request (make payment)</a> <br> <a href="/loadCustomers">Load Customers (this is an admin utility function) </a> <br>  <br> Please see the <a href="https://apiexplorersandbox.openbankproject.com">API Explorer</a> for the full list of API calls available.')
+});
+
+
+app.get('/getCurrentUser', function(req, res){
+  consumer.get(apiHost + "/obp/v2.1.0/users/current",
+  req.session.oauthAccessToken,
+  req.session.oauthAccessTokenSecret,
+  function (error, data, response) {
+      var parsedData = JSON.parse(data);
+      res.status(200).send(parsedData)
+  });
+});
+
+
+app.get('/getMyAccounts', function(req, res){
+  consumer.get(apiHost + "/obp/v2.1.0/my/accounts",
+  req.session.oauthAccessToken,
+  req.session.oauthAccessTokenSecret,
+  function (error, data, response) {
+      var parsedData = JSON.parse(data);
+      res.status(200).send(parsedData)
+  });
+});
+
+
+app.get('/createTransactionRequest', function(req, res){
+  
+
+  var template = "./template/createTransactionRequest.pug";
+  var options = { transactionRequestType :"SANDBOX_TAN"}; 
+  var html = pug.renderFile(template, options);
+
+
+  consumer.get(apiHost + "/obp/v2.1.0/my/accounts",
+  req.session.oauthAccessToken,
+  req.session.oauthAccessTokenSecret,
+  function (error, data, response) {
+      var parsedData = JSON.parse(data);
+      res.status(200).send(html)
+  });
+});
+
+
+
+app.post('/createTransactionRequest', urlencodedParser, function(req, res){
+  
+  var template = "./template/createTransactionRequest.pug";
+  
+  if (!req.body) return res.sendStatus(400)
+  
+  var fromBankId = req.body.from_bank_id;
+  var fromAccountId = req.body.from_account_id;
+
+  var toBankId = req.body.to_bank_id;
+  var toAccountId = req.body.to_account_id;
+
+  var currency = req.body.currency;
+  var amount = req.body.amount;
+
+  var description = req.body.description;
+
+
+  var transactionRequestType = req.body.transaction_request_type;
+
+  console.log("transactionRequestType is: " + transactionRequestType);
+
+
+  if (transactionRequestType.length == 0){
+    transactionRequestType = "SANDBOX_TAN";
+  }
+
+
+  // Build the body that we will post
+  var toObj = {"bank_id": toBankId, "account_id": toAccountId};
+  var valueObj = {"currency":currency, "amount":amount};
+
+  var detailsObj = {"to": toObj, "value": valueObj, "description": description}
+
+  var details = JSON.stringify(detailsObj)
+
+  console.log("details is: " + details);
+
+
+  var viewId = "owner"  
+
+  var apiHost = config.apiHost
+
+  var postUrl = apiHost + "/obp/v2.1.0/banks/" + fromBankId + "/accounts/" + fromAccountId + "/" + viewId + "/transaction-request-types/" + transactionRequestType + "/transaction-requests";
+
+  console.log("postUrl is " + postUrl);
+
+  consumer.post(postUrl,
+  req.session.oauthAccessToken,
+  req.session.oauthAccessTokenSecret,
+  details, // This is the body of the request
+  "application/json", // Must specify this else will get 404
+  function (error, data, response) {
+
+      var error = JSON.stringify(error)
+
+      console.log("error is: " + error)
+      console.log("data is: " + data)
+      console.log("response is: " + response)
+
+
+        try {
+          var parsedData = JSON.parse(data);
+          console.log("parsedData is: " + parsedData)
+          message = ""
+        } catch (err) {
+            // handle the error safely
+            console.log(err)
+            message = "Something went wrong creating a transaction request - did you supply the correct values?"
+        }
+
+      var options = {"error": error,
+                     "postUrl" : postUrl, 
+                     "fromBankId": fromBankId,
+                     "fromAccountId": fromAccountId,
+                     "toBankId": toBankId,
+                     "toAccountId" : toAccountId,
+                     "currency" : currency,
+                     "transactionRequestType" : transactionRequestType,
+                     "details": details,
+                     "data": data};   
+
+      var html = pug.renderFile(template, options) 
+
+      res.status(200).send(html)
+  });
+});
+
+
+// Loop through a Customers file, find the User matching email, Post the customer (which links to the User)
+app.get('/loadCustomers', function(req, res) {
+
+    var template = "./template/loadCustomers.pug";
+
+    // Location of customer file is stored in filesConfig.json like this:
+    //
+    // {
+    // "customerFile": "/path-to/OBP_sandbox_customers_pretty.json", 
+    // "sandboxFile": "/path-to/OBP_sandbox_pretty.json"
+    // }
+
+    var filesConfig = require('./filesConfig.json');
+
+    var customers = require(filesConfig.customerFile);
+
+
+    console.log('before customer loop. There are ' + customers.length + ' customers.')
+
+
+    customers.forEach(function processCustomer(customer) {
+
+            var usersByEmailUrl = apiHost + '/obp/v2.1.0/users/' + customer.email;
+            console.log('url to call: ' + usersByEmailUrl)
+
+            // get user by email
+            consumer.get(usersByEmailUrl,
+                req.session.oauthAccessToken,
+                req.session.oauthAccessTokenSecret,
+                function getUserForCustomer(error, data) {
+                    if (error) return console.log(error);
+                    var usersData = JSON.parse(data);
+                    console.log('usersData is: ' + JSON.stringify(usersData))
+                    var userId = usersData.users[0].user_id
+                    console.log('I got userId: ' + userId)
+                    console.log('I got customer with email , number : ' + customer.email + ' , ' + customer.customer_number)
+                    customerToPost = {
+                        "user_id": userId,
+                        "customer_number": customer.customer_number,
+                        "legal_name": customer.legal_name,
+                        "mobile_phone_number": customer.mobile_phone_number,
+                        "email": customer.email,
+                        "face_image": customer.face_image,
+                        "date_of_birth": customer.date_of_birth,
+                        "relationship_status": customer.relationship_status,
+                        "dependants": customer.dependants,
+                        "dob_of_dependants": customer.dob_of_dependants,
+                        "highest_education_attained": customer.highest_education_attained,
+                        "employment_status": customer.employment_status,
+                        "kyc_status": customer.kyc_status,
+                        "last_ok_date": customer.last_ok_date
                     }
 
-                    oa.get('https://apisandbox.openbankproject.com/oauth/token',
-                           oAuthAccessToken,
-                           oAuthAccessTokenSecret,
-                           function (error, twitterResponseData, result) {
-                               if (error) {
-                                   console.log(error)
-                                   res.end(JSON.stringify(error));
-                                   return;
-                               }
-                               try {
-                                   console.log(JSON.parse(twitterResponseData));
-                               } catch (parseError) {
-                                   console.log(parseError);
-                               }
-                               console.log(twitterResponseData);
-                               response.end(twitterResponseData);
-                           });
-                };
+                    console.log('customerToPost: ' + JSON.stringify(customerToPost))
 
-                oa.getOAuthAccessToken(urlObj.query.oauth_token, oAuthTokenSecret,
-                                       urlObj.query.oauth_verifier,
-                                       getOAuthRequestTokenCallback);
+                    var postCustomerUrl = apiHost + '/obp/v2.1.0/banks/' + customer.bank_id + '/customers';
 
-            }
-        };
-        handlers[urlObj.pathname](request, response);
-    })
+                    console.log('postCustomerUrl: ' + postCustomerUrl)
 
-}).listen(3000);
-app.start(process.env.PORT || 3000);
+
+                    consumer.post(postCustomerUrl,
+                        req.session.oauthAccessToken,
+                        req.session.oauthAccessTokenSecret,
+                        JSON.stringify(customerToPost), // This is the body of the request
+                        "application/json", // Must specify this else will get 404
+                        function (error, data) {
+                            if (error) return console.log(error);
+                            var parsedData = JSON.parse(data);
+                            console.log('response from postCustomerUrl: ' + JSON.stringify(parsedData))
+                            
+                        }); // End post customer
+
+                }); // End get user by email
+        
+    }); // End Customer loop
+
+
+    var options = {
+        "countCustomers": customers.length
+    };
+    var html = pug.renderFile(template, options);
+
+    res.status(200).send(html)
+
+});
+
+
+
+// Create Entitlements for user (e.g. loop through banks)
+app.get('/createEntitlements', function(req, res) {
+
+    var template = "./template/simple.pug"
+
+    // Location of sandbox file is stored in filesConfig.json like this:
+    //
+    // {
+    // "customerFile": "/path-to/OBP_sandbox_customers_pretty.json", 
+    // "sandboxFile": "/path-to/OBP_sandbox_pretty.json"
+    // }
+
+    var dataConfig = require('./filesConfig.json')
+
+    var sandbox = require(dataConfig.sandboxFile)
+
+    var banks = sandbox.banks
+
+
+    console.log('before loop. There are ' + banks.length + ' banks.')
+
+
+    // {
+    // "userId": "asiodiuiof35234" 
+    // }
+
+    var miscConfig = require('./miscConfig.json')
+
+    var userId = miscConfig.userId
+
+    banks.forEach(function processCustomer(bank) {
+
+            var postUrl = apiHost + '/obp/v2.1.0/users/' + userId + '/entitlements';
+            console.log('url to call: ' + postUrl)
+
+            //var postBody = {"bank_id":bank.id, "role_name":"CanCreateCustomer"}
+            var postBody = {"bank_id":bank.id, "role_name":"CanCreateUserCustomerLink"}
+
+            consumer.post(postUrl,
+                req.session.oauthAccessToken,
+                req.session.oauthAccessTokenSecret,
+                JSON.stringify(postBody), // This is the body of the request
+                "application/json", // Must specify this else will get 404
+                function getUserForCustomer(error, data) {
+                    if (error) return console.log(error);
+                    var data = JSON.parse(data);
+                    console.log('data is: ' + JSON.stringify(data))
+                }); // End POST        
+    }); // End Loop
+
+
+    var options = {
+        "count": banks.length
+    };
+    var html = pug.renderFile(template, options);
+
+    res.status(200).send(html)
+
+});
+
+
+
+
+
+
+
+
+app.get('*', function(req, res){
+  res.redirect('/connect');
+});
+
+app.listen(8085);
